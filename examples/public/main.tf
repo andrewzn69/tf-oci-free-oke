3c6a18f0f463aa519d231bbd5256beca05c75869
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-19 01:34:10 +0200

    add missing node and tag vars to public oke example

--- examples/public/main.tf
+++ examples/public/main.tf
@@ -22,9 +22,16 @@ module "oke" {
   control_plane_type          = "public"
   control_plane_allowed_cidrs = var.control_plane_allowed_cidrs
 
-  create_bastion = false
-
+  node_count               = var.node_count
+  node_ocpus               = var.node_ocpus
+  node_memory_gb           = var.node_memory_gb
+  node_boot_volume_size_gb = var.node_boot_volume_size_gb
   node_data_volume_size_gb = var.node_data_volume_size_gb
 
+  install_flannel = true
+  create_bastion  = false
+
   ssh_public_key = var.ssh_public_key
+
+  freeform_tags = var.freeform_tags
 }
