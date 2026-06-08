cfe92a1464738b4fad0ffbaa8d7f87733e960409
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-06-08 23:19:04 +0200

    bump oci provider constraint to allow 8.14+ versions

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 8.14.0"
+      version = "~> 8.14"
     }
     http = {
       source  = "hashicorp/http"
